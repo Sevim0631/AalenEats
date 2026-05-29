d391bee201e8024700ee67504f71a971a07c6492
Sevim0631 <sevim.mistikoglu@studmail.htw-aalen.de>
2026-05-29 14:11:49 +0200

    remove comments

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from gui import AalenEatsApp
 
 
 def main():
-    # 1. Daten dynamisch aus dem 'data'-Unterordner laden
     loaded_restaurants = load_data_from_csv("data/restaurants.csv")
 
-    # 2. Tkinter Instanz initialisieren
     root = tk.Tk()
 
-    # 3. GUI mit den geladenen Daten starten
     app = AalenEatsApp(root, loaded_restaurants)
 
-    # 4. App-Schleife starten
     root.mainloop()
 
 
